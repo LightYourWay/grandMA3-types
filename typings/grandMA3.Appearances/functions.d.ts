c6ab288f4e5582679f5d2b61bba19725a8fd5058
Lukas Runge <login@lukasrunge.de>
2022-10-11 18:02:55 +0200

    ✨ added types for appearance color

--- typings/grandMA3.Appearances/functions.d.ts
+++ typings/grandMA3.Appearances/functions.d.ts
@@ -3,4 +3,12 @@ type Appearances = Obj<ShowData, Appearance> & Appearance[] & { [index: string]:
 type Appearance = Obj<Appearances, null> &
 	null[] & { [index: string]: null } & {
 		Image: Image;
+		ImageR: number;
+		ImageG: number;
+		ImageB: number;
+		ImageAlpha: number;
+		BackR: number;
+		BackG: number;
+		BackB: number;
+		BackAlpha: number;
 	};
