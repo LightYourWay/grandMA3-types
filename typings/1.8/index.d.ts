cdc4d240f657df54b2dd19b5a0c021c79814df8e
Lukas Runge <login@lukasrunge.de>
2023-05-06 23:43:17 +0200

    😅 first attempt to making types "version aware"

---
+++ typings/1.8/index.d.ts
@@ -0,0 +1,4 @@
+/** @noSelf **/
+declare interface MA3_v1_8 extends MA3_v_shared {
+    methodB(): number;
+}
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,3 @@
-import './grandMA3';
-import './grandMA3.DataPool';
-import './grandMA3.Patch';
-import './grandMA3.UserProfile';
-import './grandMA3.Appearances';
-import './grandMA3.lua';
-import './lua.ftp';
+import "./shared"
+import "./1.8"
+import "./1.9"
